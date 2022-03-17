a0a17cf14b8f92e6491f7390c561a06cdb275a4e
StakeOut92 <inozemtcew@gmail.com>
2022-03-17 18:48:07 +0300

    Add classes according HW 16

---
+++ src/test/java/utilities/TestListeners.java
@@ -0,0 +1,18 @@
+package utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import tests.BaseTest;
+
+
+public class TestListeners implements ITestListener {
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+        Object currentClass = iTestResult.getInstance();
+        WebDriver webDriver = ((BaseTest) currentClass).getDriver();
+        AllureService allureService = new AllureService();
+        allureService.takeScreenshot(webDriver);
+    }
+}
